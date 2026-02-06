9f03964f4b9df83d96853ff148cf135742924c85
RohitMajithia <majithiarohit2001@gmail.com>
2026-02-06 11:30:42 +0530

    feat(api): add global exception handling
    Summary: add global exception handling.

---
+++ Backend/ElectronicShop.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using ElectronicShop.API.Common;
+using System.Net;
+using System.Text.Json;
+
+namespace ElectronicShop.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context) {
+            try
+            {
+                await _next(context);
+            }
+            catch (NotFoundException ex)
+            {
+                await HandleException(context, HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (Exception)
+            {
+                await HandleException(context, HttpStatusCode.InternalServerError,
+                    "Something went wrong. Please try again later.");
+            }
+        }
+
+        private static async Task HandleException(HttpContext context,HttpStatusCode statusCode,string message)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            var respone = new
+            {
+                statusCode = context.Response.StatusCode,
+                message
+            };
+            await context.Response.WriteAsync(JsonSerializer.Serialize(respone));
+        }
+    }
+}
